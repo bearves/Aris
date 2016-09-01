a1279b6f5bdfe767cf4a57f4152baf23d862948d
hex <hex@hex.com>
2016-09-01 14:54:07 +0800

    modify start sh since IMU driver is not needed

--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-chmod +x /usr/aris2/sh/install_xsens_driver.sh
-/usr/aris2/sh/install_xsens_driver.sh
+# since we have updated the linux kernel, xsens IMU can be automatically recognized by the system
+#chmod +x /usr/aris2/sh/install_xsens_driver.sh
+#/usr/aris2/sh/install_xsens_driver.sh
 chmod +x /usr/aris2/sh/start_ethercat_master.sh
 /usr/aris2/sh/start_ethercat_master.sh
