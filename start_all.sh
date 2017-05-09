612b39c27bc3ddf004f6a8877f2e0eaf2156b42c
hex <hex@hex.com>
2017-05-09 17:17:35 +0800

    adapt for test environment

--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-chmod +x /usr/aris/sh/install_xsens_driver.sh
-/usr/aris/sh/install_xsens_driver.sh
+# since we now use Kernel 3.18 which has a built-in xsens driver,
+# the following script is no longer required.
+
+# chmod +x /usr/aris/sh/install_xsens_driver.sh
+# /usr/aris/sh/install_xsens_driver.sh
+
+
 chmod +x /usr/aris/sh/start_ethercat_master.sh
 /usr/aris/sh/start_ethercat_master.sh
